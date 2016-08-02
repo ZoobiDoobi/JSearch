7e80ec4058d1357195d9681e7f0396b9155a4f5d
Zubair <Zubair@ZUBAIR-PC>
2016-08-02 08:27:44 +0500

    Add project files.

---
+++ JSearch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JSearch.MvcApplication" Language="C#" %>
---
+++ JSearch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JSearch.Startup))]
+namespace JSearch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
